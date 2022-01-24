f2c1aef2b5da22eef91d85008d640e81c68ca7de
alexspearsi <strelchenko.aleksandr@yandex.ru>
2022-01-24 06:36:20 +0300

    fix: finished

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 
 // You should implement your task here.
 
-module.exports = function towelSort (matrix) {
-  return [];
+module.exports = function towelSort(matrix) {
+  if (!Array.isArray(matrix)) {
+    return [];
+  } else {
+    return matrix.reduce((sum, arr, index) => (index % 2 == 0) ? sum.concat(arr) : sum.concat(arr.reverse()), []);
+  }
 }
